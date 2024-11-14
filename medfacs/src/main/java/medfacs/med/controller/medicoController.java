c9c282dcc00f548ed40da936d349c75183eebe52
SergioLuiz10 <sergioluizteixeira12345@gmail.com>
2024-11-14 10:47:04 -0300

    listagem dos medicos com os dados do json feita

--- medfacs/src/main/java/medfacs/med/controller/medicoController.java
+++ medfacs/src/main/java/medfacs/med/controller/medicoController.java
@@ -1,16 +1,15 @@
 package medfacs.med.controller;
 
-
 import jakarta.validation.Valid;
 import medfacs.med.model.dadosCadastroMed;
+import medfacs.med.model.dadosListagemMed;
 import medfacs.med.model.medico;
 import medfacs.med.model.medicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medico")
@@ -21,7 +20,15 @@ public class medicoController {
 
     @PostMapping
     @Transactional
-    public void cadastro(@RequestBody @Valid dadosCadastroMed dados){
+    public void cadastro(@RequestBody @Valid dadosCadastroMed dados) {
         repository.save(new medico(dados));
     }
+
+    @GetMapping
+    public List<dadosListagemMed> listagem() {
+        return repository.findAll().stream().map(dadosListagemMed::new).toList();
+
+    }
+
+
 }
---
+++ medfacs/src/main/java/medfacs/med/model/dadosListagemMed.java
@@ -0,0 +1,12 @@
+package medfacs.med.model;
+
+public record dadosListagemMed(
+        String nome,
+        String email,
+        String crm,
+        especialidade especialidade){
+
+    public dadosListagemMed(medico med) {
+        this(med.getNome(), med.getEmail(), med.getCrm(), med.getEspecialidade());
+    }
+}
